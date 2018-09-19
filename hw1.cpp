a25f427da4d6ee27c05a7ea3372f1e0340e7eb2f
chensh236 <1466196675@qq.com>
2018-09-19 11:02:42 +0800

    upload the final files

--- hw1.cpp
+++ hw1.cpp
@@ -8,34 +8,40 @@
 
 #include "CImg.h"
 #include "hw1.h"
+#include <math.h>
 #include <iostream>
 using namespace cimg_library;
 using namespace std;
 
-image_process::image_process(){
+image_process::image_process()
+{
     int width = 0;
     int height = 0;
 }
 
-image_process::image_process(string str){
+image_process::image_process(string str)
+{
     load_img(str);
 }
 
 // load the picture
-void image_process::load_img(string str){
+void image_process::load_img(string str)
+{
     // load the bmp picture
     SrcImg.load_bmp(str.c_str());
-    
+
     //gain the width and height of the picture
     width = SrcImg._width;
     height = SrcImg._height;
 }
 
-void image_process::save_img(string str, CImg<unsigned char> img){
-    img.save(str.c_str());
+void image_process::save_img(string str)
+{
+    SrcImg.save(str.c_str());
 }
 
-void image_process::test_cimg(){
+void image_process::test_cimg()
+{
     // initial a new pic
     CImg<unsigned char> TempImg(width, height, 1, 1, 0);
     SrcImg.display();
@@ -44,16 +50,106 @@ void image_process::test_cimg(){
     // process every pixel
     cimg_forXY(SrcImg, x, y)
     {
-        if(SrcImg(x, y, 0) == 102 && SrcImg(x, y, 1) == 102 && SrcImg(x, y, 2) == 102)
+        if (SrcImg(x, y, 0) == 102 && SrcImg(x, y, 1) == 102 && SrcImg(x, y, 2) == 102)
             TempImg(x, y) = 255;
     }
     // display the picture
     TempImg.display();
     // save the picture
     TempImg.save("Thread.bmp");
-    
 }
 
-void image_process::display(){
+void image_process::display()
+{
+    SrcImg.display();
+}
+
+void image_process::task1()
+{
+    cimg_forXY(SrcImg, x, y)
+    {
+        if (SrcImg(x, y, 0) == 255 && SrcImg(x, y, 1) == 255 && SrcImg(x, y, 2) == 255)
+        {
+            SrcImg(x, y, 0) = 255;
+            SrcImg(x, y, 1) = 0;
+            SrcImg(x, y, 2) = 0;
+        }
+        else if (SrcImg(x, y, 0) == 0 && SrcImg(x, y, 1) == 0 && SrcImg(x, y, 2) == 0)
+        {
+            SrcImg(x, y, 0) = 0;
+            SrcImg(x, y, 1) = 255;
+            SrcImg(x, y, 2) = 0;
+        }
+    }
+    SrcImg.display();
+}
+
+void image_process::task2()
+{
+    cimg_forXY(SrcImg, x, y)
+    {
+        if ((x - 50) * (x - 50) + (y - 50) * (y - 50) <= 900)
+        {
+            SrcImg(x, y, 0) = 0;
+            SrcImg(x, y, 1) = 0;
+            SrcImg(x, y, 2) = 255;
+        }
+    }
+    SrcImg.display();
+}
+
+void image_process::task3()
+{
+    cimg_forXY(SrcImg, x, y)
+    {
+        if ((x - 50) * (x - 50) + (y - 50) * (y - 50) <= 9)
+        {
+            SrcImg(x, y, 0) = 255;
+            SrcImg(x, y, 1) = 255;
+            SrcImg(x, y, 2) = 0;
+        }
+    }
+    SrcImg.display();
+}
+
+void image_process::task4()
+{
+    /*三角函数法*/
+    /*-----------------------------------------*/
+    cimg_forY(SrcImg, y)
+    {
+        for (int x = 0; x <= floor(100 * cos(0.6108653)); x++)
+        {
+            if (y == round(x * tan(0.6108653)))
+            {
+                SrcImg(x, y, 0) = 0;
+                SrcImg(x, y, 1) = 0;
+                SrcImg(x, y, 2) = 255;
+            }
+        }
+    }
+    
+
+    /*DDA*/
+    /*-----------------------------------------*/
+    // float grad_x, grad_y, point_x, point_y;
+    // // k = end.x - start.x
+    // int minus_x = floor(100 * cos(0.6108653)) - 0;
+    // int minus_y = floor(100 * sin(0.6108653)) - 0;
+    // int k = abs(minus_x);
+    // //end.y - start.y
+    // if (abs(minus_y) > k)    k = abs(minus_y);
+    // grad_x = (float)(minus_x) / k;
+    // grad_y = (float)(minus_y) / k;
+    // point_x = 0;
+    // point_y = 0;
+
+    // for(int i = 0; i < k - 1; i++){
+    //     SrcImg(point_x, point_y, 0) = 0;
+    //     SrcImg(point_x, point_y, 1) = 0;
+    //     SrcImg(point_x, point_y, 2) = 255;
+    //     point_x += grad_x;
+    //     point_y += grad_y;
+    // }
     SrcImg.display();
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,73 @@
 #include "hw1.h"
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    /*通过单实例进行测试，输出图片*/
+    /*-------------------------------------------*/
+    /*Created: 2018.9.16*/
+    //display the img
     image_process instance("1.bmp");
-    instance.test_cimg();
-    std::cout << "Example Test 1\n";
+    instance.display();
+    //white->red; black->green
+    instance.task1();
+    // circle: R:30 circle(50, 50)
+    instance.task2();
+
+    // circle : R:3 circle(50, 50)
+    instance.task3();
+
+    // a line 35 degrees
+    instance.task4();
+    instance.save_img("2.bmp");
+
+
+    /*通过不同的实例进行单独测试与CImg提供的函数进行比较*/
+    /*-------------------------------------------*/
+    /*Created: 2018.9.12*/
+    // //display the img
+    // image_process instance1("1.bmp");
+    // instance1.display();
+    // //white->red; black->green
+    // instance1.task1();
+    // instance1.save_img("2.bmp");
+
+    // image_process instance2("1.bmp");
+    // // circle: R:30 circle(50, 50)
+    // instance2.task2();
+    // instance2.save_img("3.bmp");
+    // //Compare
+    // CImg<unsigned char> testTask2("1.bmp");
+    //  unsigned char blue[] = { 0, 0, 255 };
+    // testTask2.draw_circle(50, 50, 30, blue);
+    // testTask2.display();
+    // testTask2.save("3_1.bmp");
+
+    // image_process instance3("1.bmp");
+    // // circle : R:3 circle(50, 50)
+    // instance3.task3();
+    // instance3.save_img("4.bmp");
+    // //Compare
+    // CImg<unsigned char> testTask3("1.bmp");
+    // unsigned char yellow[] = { 255, 255, 0 };
+    // testTask3.draw_circle(50, 50, 3, yellow);
+    // testTask3.display();
+    // testTask3.save("4_1.bmp");
+
+
+    // image_process instance4("1.bmp");
+    // // a line 35 degrees
+    // instance4.task4();
+    // instance4.save_img("5.bmp");
+    // //Compare
+    // CImg<unsigned char> testTask4("1.bmp");
+    // int x1 = floor(cos(0.6108653) * 100);
+    // int y1 = floor(sin(0.6108653) * 100);
+    // testTask4.draw_line(0,0,x1, y1, blue, 1);
+    // cout << x1 << "       "<< y1<<endl;
+    // testTask4.display();
+    // testTask4.save("5_1.bmp");
+
 
     return 0;
 }
